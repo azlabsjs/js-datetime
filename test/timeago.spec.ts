79bf08264a68d591638a6f0c40cb88959ef010b3
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-02-17 16:40:49 +0000

    Updated timeago test file

--- test/timeago.spec.ts
+++ test/timeago.spec.ts
@@ -13,11 +13,12 @@ describe('TimeAgo utilities tests', () => {
 
   it('new TimeAgo().format(date, "en-US") should returns a message depending on the current date', () => {
     const date = new Date('2022-02-10T00:00:00');
-    const timeago = new TimeAgo().format(date, 'en-US');
     if (JSDate.isPast(date)) {
-      expect(timeago.includes('ago')).toEqual(true);
+      expect(new TimeAgo().format(date, 'en-US').includes('ago')).toEqual(true);
     } else {
-      expect(timeago.includes('after')).toEqual(true);
+      expect(new TimeAgo().format(date, 'en-US').includes('after')).toEqual(
+        true
+      );
     }
   });
 });
